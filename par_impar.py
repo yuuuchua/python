7a980522259a2ce9f8747ea3c87704825d66528f
yuuuchua <marialicermartins44@gmail.com>
2026-02-10 15:16:47 -0300

    abc

---
+++ par_impar.py
@@ -0,0 +1,13 @@
+# 10_02_2026 py.prog04: classifica numeros como par ou impar
+# por yuuuchua
+
+def par_ou_impar(n):
+    if n % 2 == 0:
+        return "par"
+    else:
+        return "ímpar"
+
+n = int(input("Digite o número: "))
+
+for i in range(1, 1+n):
+    print(i, "é", par_ou_impar(i))
